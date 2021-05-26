5b2a49d00d2b33f46393ee9bed98883e6c3f5d69
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-26 16:13:25 +0300

    Task 3 completed

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,11 +1,17 @@
-﻿namespace task3
+﻿using System;
+
+namespace task3
 {
     internal static class Program
     {
         private static void Main()
         {
-            //Используя динамические и анонимные типы данных, создайте Англо-Русский словарь на 10 слов и
-            //выведите на экран его содержание.
+            var dictionary = new
+                {englishWords = new[] {"one", "two", "three"}, russianWords = new[] {"один", "два", "три"}};
+            for (var i = 0; i < dictionary.englishWords.Length; i++)
+            {
+                Console.WriteLine($"{dictionary.englishWords[i]}: {dictionary.russianWords[i]}");
+            }
         }
     }
 }
